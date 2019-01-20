14670f4d041171861f5782fe1925a648d4c2ded0
Ugputu <eugene@iteye.ru>
2019-01-20 23:54:01 +0300

    Remove unused

--- src/fileinfo.php
+++ src/fileinfo.php
@@ -3,31 +3,17 @@ namespace dsda\fileinfo;
 
 class fileinfo {
 	
-	public $result = false;
 	private $input_files = [];
 		
-	function __construct(array $files=[], array $data=[]) {
+	function __construct(array $files=[]) {
 		
-		//$this->catcher = new \dsda\catcher\catcher();
-		
-		// in - array of files
-		//      video, audio
-		//      video
-		//      image file
-		
-		// data - post data with trim, blur, task_type, tags, source, description, location
-		
-		// out - true if file correct and task accepted
-		//       error message if file not correct or something wrong
-		
-
 		// check exist files
 		foreach($files as $key=>$file) {
 			if (!file_exists($file)) {
-				throw new \Exception("FilesManipulator: Input file - ".$file." not found!", 0);
+				throw new \Exception("FileInfo: Input file - ".$file." not found!", 0);
 			}
 			if (!is_readable($file)) {
-				throw new \Exception("FilesManipulator: Input file - ".$file." not readable!", 0);
+				throw new \Exception("FileInfo: Input file - ".$file." not readable!", 0);
 			}
 		}
 		
@@ -35,37 +21,14 @@ class fileinfo {
 		foreach($files as $key=>$file) {
 			$media_type = $this->get_media_type($file);
 			if ($media_type==false) {
-				throw new \Exception("FilesManipulator: file - ".$file." is unknown type!", 0);
+				throw new \Exception("FileInfo: file - ".$file." is unknown type!", 0);
 			}
 			$this->input_files[] = ['path'=>$file, 'type'=>$media_type];
 			unset($media_type);
 		}
-		
-		// generate filename ($this->output_file)
-		$this->set_output_name();
-		
-		// add post to db
-		//XXX: тут закончил
-		$config = new \dsda\config\config();
-		$db = new \dsda\dbconnector\dbconnector($config);
-		
-		$post_id = $db->query("INSERT INTO `post` VALUES (NULL, )");
-		
-		$this->result = $post_id;
-		
-		// create task for files with post_id
+
 	}
-	
-	
-	private function set_output_name(){
-		if ($this->input_files[0]['type']=='image') {
-			$ext = 'jpg';
-		} else {
-			$ext = 'mp4';
-		}
-		$this->output_file = explode(" ",microtime(false))[1].str_replace("0.","",explode(" ",microtime(false))[0]).'.'.$ext;
-	}
-	
+
 	private function get_media_type($file_path){
 		$container = strtolower(pathinfo($file_path, PATHINFO_EXTENSION));
 		$name = strtolower(pathinfo($file_path, PATHINFO_FILENAME));
